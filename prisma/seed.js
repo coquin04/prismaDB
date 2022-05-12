550dbfcf5edce7085ad1ff93685e5a6db9371587
jorge-juareza <coquin04@gmail.com>
2022-05-12 15:12:04 -0500

    Add: commanders trough seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -50,4 +50,58 @@ const prisma = new PrismaClient();
   } finally {
     await prisma.$disconnect();
   }
+
+  // Ajolocommanders
+  try {
+    const coopa = await prisma.commander.upsert({
+      where: { name: "Coopa" },
+      update: {},
+      create: {
+        name: "Coopa",
+        lang: "ES",
+        missionCommander: "ajolocommander",
+        enrollments: 3,
+      },
+    });
+
+    const coopa1 = await prisma.commander.upsert({
+      where: { name: "Coopa1" },
+      update: {},
+      create: {
+        name: "Coopa1",
+        lang: "EN",
+        missionCommander: "ajolocommander1",
+        enrollments: 6,
+      },
+    });
+
+    const coopa2 = await prisma.commander.upsert({
+      where: { name: "Coopa2" },
+      update: {},
+      create: {
+        name: "Coopa2",
+        lang: "ES",
+        missionCommander: "ajolocommander2",
+        enrollments: 9,
+      },
+    });
+
+    const coopa3 = await prisma.commander.upsert({
+      where: { name: "Coopa3" },
+      update: {},
+      create: {
+        name: "Coopa3",
+        lang: "EN",
+        missionCommander: "ajolocommander3",
+        enrollments: 12,
+      },
+    });
+
+    console.log("Create 3 commanders");
+  } catch (e1) {
+    console.error(e1);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
 })();
